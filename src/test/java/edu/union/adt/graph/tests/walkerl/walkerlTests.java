e737db594a6def82510d88a76619693855cd41eb
walkerl <walkerl@union.edu>
2021-01-31 00:00:20 -0500

    added test #2 removeEdgeSuccessful()

--- src/test/java/edu/union/adt/graph/tests/walkerl/walkerlTests.java
+++ src/test/java/edu/union/adt/graph/tests/walkerl/walkerlTests.java
@@ -27,7 +27,7 @@ public class walkerlTests {
     @Test
     public void removeEdgeFromNoEdges() {
         String from = "Hello!";
-        String to = "Goodbye";
+        String to = "Goodbye!";
         tester1.removeEdge(from, to);
         assertEquals("removing an edge from an empty graph changes nothing",
                 tester1.numEdges(), 0);
@@ -35,5 +35,23 @@ public class walkerlTests {
         tester1.removeEdge(to,from);
         assertEquals("removing the backwards form of an edge does not remove the actual edge",
                 tester1.numEdges(), 1);
+    }
+
+    @Test
+    public void removeEdgeSuccessful() {
+      String from = "Hello!";
+      String to = "Goodbye!";
+      tester1.addEdge(from, to);
+      assertEquals("confirming the edge was successfully added",
+                tester1.numEdges(), 1);
+      tester1.removeEdge(from, to);
+      assertEquals("removing the edge should reduce numEdges by 1",
+                tester1.numEdges(), 0);
+      assertEquals("removing the edge should NOT reduce the numVertices",
+                tester1.numVertices(), 2);
+      assertTrue("removing the edge should NOT remove the source vertex",
+                tester1.contains(from));
+      assertTrue("removing the edge should NOT remove the destination vertex",
+                tester1.contains(to));
     }
 }
